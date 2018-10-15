a99e1a379f6caf5f52f59b8bfaebda0d85857011
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-10-15 12:20:27 -0700

    Add createStore so common code is re-usable

--- reducer.js
+++ reducer.js
@@ -1,28 +1,40 @@
-// add code snippets from README
-//let state = {count: 0}
-let state;
-let button = document.getElementById('button');
- 
-button.addEventListener('click', function(){
-    dispatch({type: 'INCREASE_COUNT'})
-})
-
-function reducer(state = {count: 0}, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1};
-    default:
+function createStore(reducer) {
+    let state;
+   
+    function dispatch(action) {
+      state = changeCount(state, action);
+      render();
+    }
+   
+    function getState() {
       return state;
-  }
-}
-
-function dispatch(action){
-    state = reducer(state, action);
-    render();
-}
-
-
-function render(){
+    }
+   
+    return {
+      dispatch,
+      getState
+    };
+  };
+   
+  function changeCount(state = { count: 0 }, action) {
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return { count: state.count + 1 };
+   
+      default:
+        return state;
+    }
+  };
+   
+  function render() {
     let container = document.getElementById('container');
-    container.textContent = state.count;
-}
+    container.textContent = store.getState().count;
+  };
+   
+  let store = createStore(changeCount);
+  store.dispatch({ type: '@@INIT' });
+  let button = document.getElementById('button');
+   
+  button.addEventListener('click', function() {
+      store.dispatch({ type: 'INCREASE_COUNT' });
+  })
